9d033181eaf198ccd46a4b2b12ac41f337830445
TranGiaHuan0102 <trangiahuan2411@gmail.com>
2025-11-06 10:16:17 +0700

    Added PostgreSQL. Connection successful!

---
+++ src/main/java/com/tokuden/sql/ConfigLoader.java
@@ -0,0 +1,48 @@
+package com.tokuden.sql;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import java.io.InputStream;
+
+public class ConfigLoader {
+    private String url;
+    private String username;
+    private String password;
+    
+    public ConfigLoader() {
+        loadConfig();
+    }
+    
+    private void loadConfig() {
+        try {
+            // Load XML from resources folder
+            InputStream inputStream = getClass().getClassLoader()
+                .getResourceAsStream("com/tokuden/sql/config.properties.xml");
+            
+            
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(inputStream);
+            
+            // Get database element
+            NodeList databaseList = doc.getElementsByTagName("database");
+            Element database = (Element) databaseList.item(0);
+            
+            // Extract values
+            this.url = database.getElementsByTagName("url").item(0).getTextContent();
+            this.username = database.getElementsByTagName("username").item(0).getTextContent();
+            this.password = database.getElementsByTagName("password").item(0).getTextContent();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // Getters
+    public String getUrl() { return url; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+}
