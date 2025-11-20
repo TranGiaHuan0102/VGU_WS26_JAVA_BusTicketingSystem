a07adaa40608c9b796154eb5bb7b0fd079556fc4
TranGiaHuan0102 <trangiahuan2411@gmail.com>
2025-11-20 13:12:17 +0700

    Working insertion for 3 ticket types

--- src/main/java/com/java/tickets/Ticket.java
+++ src/main/java/com/java/tickets/Ticket.java
@@ -2,44 +2,19 @@ package com.java.tickets;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
-import java.text.ParsePosition;
-import java.text.NumberFormat;
-import package1.locations.LocationRepository;
 
 
 public abstract class Ticket implements Validatable {
-    private String student_id;
-    private String ticket_type;
-    private String location;
-    private LocalDate start_date;
+    private String id;
+    private final LocalDate start_date;
+    private final String location;
+    
     
     // Constructor
-    Ticket(String student_id, String ticket_type, String location, LocalDate date){
-        this.student_id = student_id;
-        this.ticket_type = ticket_type;
+    Ticket(String id,  LocalDate start_date, String location){
+        this.id = id;
+        this.start_date = start_date;
         this.location = location;
-        this.start_date = date;
-    }
-    
-    // Methods to verify the validity of input
-    public static String loc_verified(String location){
-        if (!LocationRepository.isValidLocation(location)){
-            throw new IllegalArgumentException("Invalid Location!");
-        }
-        return location;
-    }
-    
-    public static boolean isNumeric(String str){
-        ParsePosition pos = new ParsePosition(0);
-        NumberFormat.getInstance().parse(str, pos);
-        return str.length() == pos.getIndex();
-    }
-    
-    public static String id_verified(String id){
-        if (id.length() != 8 || !isNumeric(id)){
-            throw new IllegalArgumentException("Invalid ID!");
-        }
-        return id;
     }
     
     public static LocalDate date_verified(String date_string){
@@ -52,7 +27,11 @@ public abstract class Ticket implements Validatable {
     }
     
     // Getters:
-    LocalDate getDate(){return this.start_date;}
+    public String getID(){return this.id;}
+    public LocalDate getStartDate(){return this.start_date;}
+    public String getLocation(){return this.location;}
+    
+    
     
     public void ExpiryMessage(boolean expired){
         if (expired){
@@ -72,13 +51,6 @@ public abstract class Ticket implements Validatable {
         }
     }
     
-    void generic_ticket_info(){
-        System.out.println("Ticket Holder ID: " + student_id);
-        System.out.println("Type: " + ticket_type);
-        System.out.println("Journey: " + location + " - VGU");
-    }
-    
-    abstract void specific_ticket_info();
     
     // Interface methods - force subclasses to implement
     @Override
