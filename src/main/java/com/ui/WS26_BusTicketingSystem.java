6893099c010beb9752f01380f89be2ae209ba60c
chriscao04 <caodangtri09@gmail.com>
2025-11-21 20:18:11 +0700

    GUI completed!

---
+++ src/main/java/com/ui/WS26_BusTicketingSystem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ui;
+
+/**
+ *
+ * @author caoda
+ */
+public class WS26_BusTicketingSystem {
+
+    // The only main
+    public static void main(String[] args) {
+        MainMenu mr = new MainMenu();
+        mr.setVisible(true);
+        mr.setLocationRelativeTo(null);
+    }
+}
